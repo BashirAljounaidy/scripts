8ec77d174c42fec27f0e1f10f0524df24fcba58b
Bashir Aljounaidy <bma.963@gmail.com>
2023-08-06 18:32:47 +0200

    add extend directory

--- install.sh
+++ install.sh
@@ -10,18 +10,21 @@ else
     exit 1
 fi
 
-# Add the bin directory to the PATH
-echo 'export PATH="$HOME/Scripts/bin:$PATH"' >> "$HOME/.$myshell"rc
-
-test -f "$HOME/.$myshell"rc && source "$HOME/.$myshell"rc || echo "Could not source $HOME/.$myshell"rc || exit 1
-
-echo "Scripts installed successfully"  
 
 # Make all files in the bin directory executable
 find "$HOME/Scripts/bin" -type f | while read -r file; do
     chmod +x "$file"
 done
 
-echo "All files in the bin directory are now executable"
+# Add the bin directory to the PATH
+test -f "$HOME/.$myshell"rc && source "$HOME/.$myshell"rc && echo '."$myshell"rc founded ' ||  touch "$HOME/.$myshell"rc && echo '."$myshell"rc created ' || echo '."$myshell"rc not created ' && exit 1
+# edits 
+echo 'export PATH="$HOME/Scripts/bin:$PATH"' >> "$HOME/.$myshell"rc
+echo 'source "~/$HOME/Scripts/extend/*"' >> "$HOME/.$myshell"rc
 
 exec $myshell
+
+#check if it is work 
+command -v myecho >/dev/null && echo "os" || echo "os not found"
+#check if it is work
+command -v os >/dev/null && echo "os" || echo "os not found"
